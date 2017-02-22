eff812137c2d694e9aaf209638a155dcf80477fd
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2017-02-22 18:27:26 +0000

    1: angular2 + auth0

---
+++ app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'vQqx6EMcu8N06l2bJI6hgUDmKo5qKY2x',
+    domain: 'erikweisz.auth0.com'
+};
